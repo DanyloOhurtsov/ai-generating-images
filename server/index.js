0dd6820c39eec2236e4f6181a37b7b965e2a23b6
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2024-01-05 15:00:30 +0100

    Ai routes ready

--- server/index.js
+++ server/index.js
@@ -2,13 +2,18 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
-import connectDB from "./mongodb/connect";
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import aiRoutes from "./routes/aiRoutes.js";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use('/api/v1/post', postRoutes)
+app.use('/api/v1/ai', aiRoutes)
 
 app.get("/", async (req, res) => {
     res.send("Hello from DALL-E!");
